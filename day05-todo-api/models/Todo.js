7e55d1c01bd4f6bb872cef0ec03ef4c61fcb8f92
Nishant Bisht <nishantbisht330@gmail.com>
2026-03-20 12:04:19 +0530

    Add Day 5 Todo API files

---
+++ day05-todo-api/models/Todo.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+    },
+    completed: {
+      type: String,
+      default: false,
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("Todo", todoSchema);
